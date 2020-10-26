8c6e736d65ca0927d57143dd0752aa3da92238e7
Michael Bonner <michaelwbonner@gmail.com>
2020-10-26 13:00:14 -0600

    svgs for some sections for now

--- postcss.config.js
+++ postcss.config.js
@@ -5,7 +5,6 @@ const IS_DEVELOPMENT = process.env.NODE_ENV === "development";
 const plugins = [];
 plugins.push(tailwindcss);
 plugins.push(tailwindcss("tailwind.config.js"));
-// This is if you want to include your custom config
 
 if (!IS_DEVELOPMENT) {
   const purgecss = require("@fullhuman/postcss-purgecss");
