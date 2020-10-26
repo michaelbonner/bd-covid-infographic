1773495a5b7ee0fb74896feba1a362b5cb85e7e0
Michael Bonner <michaelwbonner@gmail.com>
2020-10-25 19:34:32 -0600

    more work on first orange section

--- postcss.config.js
+++ postcss.config.js
@@ -4,7 +4,7 @@ const IS_DEVELOPMENT = process.env.NODE_ENV === "development";
 
 const plugins = [];
 plugins.push(tailwindcss);
-// plugins.push(tailwindcss('tailwind.config.js'))
+plugins.push(tailwindcss("tailwind.config.js"));
 // This is if you want to include your custom config
 
 if (!IS_DEVELOPMENT) {
